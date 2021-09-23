3e473ee7251936ac28127c42d3cf8e93ac603ac6
RamanSlinka <romuch1986@gmail.com>
2021-09-23 20:24:41 +0300

    created EventPage + Calendar page(ant design) + logic (action-creators, types, thunk) created API (save  in .JSON and localstorage)
    ... some refactors

--- src/store/reducers/auth/action-creators.ts
+++ src/store/reducers/auth/action-creators.ts
@@ -1,7 +1,7 @@
 import {IUser} from "../../../models/IUser";
 import {AuthActionsEnum, SetAuthAction, SetErrorAction, SetIsLoadingAction, SetUserAction} from "./types";
 import {AppDispatch} from "../../index";
-import axios from "axios";
+import UserService from "../../../Api/UserService";
 
 export const AuthActionCreators = {
     setUser: (user: IUser): SetUserAction => ({type: AuthActionsEnum.SET_USER, payload: user}),
@@ -13,13 +13,14 @@ export const AuthActionCreators = {
         try {
             dispatch(AuthActionCreators.setIsLoading(true));
             setTimeout(async () => {
-                const response = await axios.get<IUser[]>('./users.json')
+                const response = await UserService.getUsers();
                 const mockUser = response.data.find(user => user.username === username && user.password === password)
                 if (mockUser) {
                     localStorage.setItem('auth', 'true')
                     localStorage.setItem('username', mockUser.username)
-                    dispatch(AuthActionCreators.setIsAuth(true))
                     dispatch(AuthActionCreators.setUser(mockUser))
+                    dispatch(AuthActionCreators.setIsAuth(true))
+
                 } else {
                     dispatch(AuthActionCreators.setError('incorrect login or password'))
                 }
